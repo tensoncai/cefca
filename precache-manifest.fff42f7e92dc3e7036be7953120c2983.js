5657fc501cb5ab84f1b231c63fc001fcae1e659c
Tenson Cai <tensoncai@Yings-Air-9.lan>
2020-06-13 21:57:02 -0500

    Updates

---
+++ precache-manifest.fff42f7e92dc3e7036be7953120c2983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297a0fa576d6976ddaf30105600a7211",
+    "url": "/cefca/index.html"
+  },
+  {
+    "revision": "dd775543864503fcb953",
+    "url": "/cefca/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fb5b8e4d9da2efde1be3",
+    "url": "/cefca/static/css/main.b8444fac.chunk.css"
+  },
+  {
+    "revision": "dd775543864503fcb953",
+    "url": "/cefca/static/js/2.f1da0863.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cefca/static/js/2.f1da0863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5b8e4d9da2efde1be3",
+    "url": "/cefca/static/js/main.d7f5d5c1.chunk.js"
+  },
+  {
+    "revision": "c1cf5fd8b158cdd76a2e",
+    "url": "/cefca/static/js/runtime-main.4b396b23.js"
+  }
+]);
