9f8f2b7a7fff7022488953c52d0cff565e727785
Tenson Cai <tensoncai@Yings-Air-9.lan>
2020-06-17 15:26:42 -0500

    cleaned up code

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,21 +5,6 @@ import '../CSS/Styling.css';
 
 class Contact extends Component {
 
-  styling = {
-    backgroundImage: `url(${'/trees.jpg'})`,
-    backgroundSize: 'cover', 
-    overflow: 'hidden',
-    backgroundPosition: 'center', 
-    backgroundRepeat  : 'no-repeat',
-    height: '400px', 
-    fontSize: '70px', 
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: '100',
-    lineHeight: '400px',
-    marginBottom: '30px'
-  }
-
   render() {
     return (
       <div>
@@ -40,7 +25,7 @@ class Contact extends Component {
           <span style={{display: 'block', fontSize: '20px'}}>(515) 233-4436</span>
         </p>
         <div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2963.916820791477!2d-93.68619538563216!3d42.02351676428242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87ee7760c7d1184b%3A0xffa59b1133eff4aa!2s4911%20Lincoln%20Way%2C%20Ames%2C%20IA%2050014!5e0!3m2!1sen!2sus!4v1592410258940!5m2!1sen!2sus" 
+          <iframe title="myFrame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2963.916820791477!2d-93.68619538563216!3d42.02351676428242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87ee7760c7d1184b%3A0xffa59b1133eff4aa!2s4911%20Lincoln%20Way%2C%20Ames%2C%20IA%2050014!5e0!3m2!1sen!2sus!4v1592410258940!5m2!1sen!2sus" 
                   style={{width: '600px', height: '450px', frameborder: '0px', border:'0px', margin: '30px'}}></iframe>
         </div>
         <Footer />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,28 +4,12 @@ import Footer from "./Footer";
 import "../CSS/Styling.css";
 
 class Home extends Component {
-
-  styling = {
-    backgroundImage: `url(${'/cross3.jpg'})`,
-    backgroundSize: 'cover', 
-    overflow: 'hidden',
-    backgroundPosition: 'center', 
-    backgroundRepeat  : 'no-repeat',
-    height: '400px', 
-    fontSize: '70px', 
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: '100',
-    lineHeight: '400px',
-    marginBottom: '30px'
-  }
   
   render() {
-
     return (
       <div className="home">
         <NavBar />
-        <div style={this.styling} className="body">
+        <div className="mainImageText" style={{backgroundImage: `url(${'/church.jpeg'})`, color: 'black'}}>
           Welcome | 欢迎
         </div>
         <Footer />
