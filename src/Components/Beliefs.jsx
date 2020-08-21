1c53c8c8b34e354981da46761fa5d1c4d0ac1582
Tenson Cai <tensoncai@gmail.com>
2020-08-21 14:17:08 -0500

    cleaned up code

--- src/Components/Beliefs.jsx
+++ src/Components/Beliefs.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-// import beliefStatement from './2019_nov_efca_sof_mandarin.pdf';
 
 class Beliefs extends Component {
 
